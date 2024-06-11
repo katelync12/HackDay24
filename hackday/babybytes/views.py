3b3ddecac40539d02fedaea1de48ace5d7c88a2b
katelync12 <katelynchen122132@gmail.com>
2024-06-11 10:30:15 -0400

    Created a basic home and login page

--- hackday/babybytes/views.py
+++ hackday/babybytes/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to a homepage or dashboard
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def home_view(request):
+    return render(request, 'home.html')
